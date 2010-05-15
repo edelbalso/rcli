d7c8c693f6e11abc6e377317164751e90e8cf498
Eduardo Del Balso <e.delbalso@gmail.com>
2010-05-15 15:44:45 -0400

    removed 's' and 'f' methods

--- lib/commands/example.rb
+++ lib/commands/example.rb
@@ -1,20 +1,15 @@
 class ExampleCommand  < Command
   
   def after_init
-    s
     # called after Command::initialize
     @description = "An example command that lists directories in a current folder"
-    f
   end
 
   def before_main
-    s
     # called right before the command's ::main() function
-    f
   end
   
   def main
-    s
     options = parse_parameters
 
     $verbose= options.verbose
@@ -28,7 +23,6 @@ class ExampleCommand  < Command
         list_fs_key(fs_key)
       end
     end
-    f
   end
 
 
