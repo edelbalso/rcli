afb5ae2a65b9b840b12339634ef6c083d4748256
Eduardo Del Balso <e.delbalso@gmail.com>
2010-09-15 00:23:24 -0400

    reorganized core includes

--- lib/rcli.rb
+++ lib/rcli.rb
@@ -4,25 +4,30 @@ require 'text'
 require 'YAML'
 require 'pp'
 require 'pathname'
+require 'core/shared/rcli_installation.rb'
 
 ### Globals
 $verbose = false # set to true by command line
 DS = File::SEPARATOR # / on unix, \ on windows. DS is a shortcut and faster to type
 
-### Application Libraries
-require 'core/traceable_factory'
-require 'core/commander'
-require 'core/global_functions'
-require 'vendor/trollop'
-
 class Rcli
 
+  include Rcli::Installation
+  
   GEM_ROOT = File.dirname(File.dirname(__FILE__))
   GEM_LIB = GEM_ROOT + DS + 'lib'
   GEM_BIN = GEM_ROOT + DS + 'bin'
-  GEM_CONFIG = GEM_ROOT + DS + 'bin' + DS + 'config'
+  GEM_CONFIG = GEM_ROOT + DS + 'lib' + DS + 'config'
   
-  APP_CONFIG = YAML.load_file(GEM_LIB + '/config/application.yaml')
+  SRC_PATH = File.expand_path('~/lib/ruby/rcli_framework')
+  APP_DOTFOLDER = File.expand_path("~" + DS + '.rcli')
+  
+  if File.exists? (APP_DOTFOLDER + DS + 'config.yml')
+    APP_CONFIG = YAML.load_file(APP_DOTFOLDER + DS + 'config.yml')
+  else
+    APP_CONFIG = YAML.load_file(GEM_LIB + '/config/application.yaml')
+  end
+
   
   @@trace_app = false
   @@script_root = 'uninitialized'
@@ -33,11 +38,10 @@ class Rcli
   def self.trace_app=(ta) ; @@trace_app = ta ; end
 end
 
-# puts Rcli::GEM_ROOT
-# puts Rcli::GEM_LIB
-# puts Rcli::GEM_BIN
-# puts Rcli::GEM_CONFIG
-# puts Rcli.script_root
-# puts Rcli.trace_app.inspect
-# pp Rcli::APP_CONFIG 
-# exit
+
+### Application Libraries
+require 'core/traceable_factory'
+require 'core/actions'
+require 'core/global_functions'
+require 'vendor/trollop'
+require 'core/commander'
