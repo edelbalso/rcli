dd5ff6520b547092b963e67662e92f6da733fa22
Eduardo Del Balso <e.delbalso@gmail.com>
2010-09-15 20:39:26 -0400

    moved installation code out of shared module.

--- lib/commands/install.rb
+++ lib/commands/install.rb
@@ -57,4 +57,32 @@ class InstallCommand  < Command
       end
     end
   end
+  
+private
+
+  def _install_rcli_base
+    if _installed_rcli_base?
+      return false
+    end
+
+    dotpath = File.expand_path('~/' + Rcli::RCLI_CONFIG['global']['hidden_dir_name'] )
+    puts "Installing to : #{dotpath}"
+    # exit
+    FileUtils.mkdir_p(dotpath + DS + 'bin')
+    FileUtils.mkdir_p(dotpath + DS + 'app_info')
+
+    if agree("Add rcli to your path to allow script installations? [yn]", true)
+      File.open(File.expand_path('~/.bash_profile'),'a') { |f|
+        f << "\n\n# rcli Installer addition on #{Time.now.inspect}. Adding appropriate PATH variables for use with rcli."
+        f << "\nexport PATH=#{dotpath}/bin:$PATH"
+        f << "\n# Finished adapting your PATH environment variable for use with rcli."
+      }
+    end
+  
+    File.open(dotpath + DS + 'config.yml', 'w') { |f|
+      f << File.read(Rcli::GEM_CONFIG + DS + 'application.yml')
+    }
+  
+
+  end
 end
--- lib/core/shared/rcli_installation.rb
+++ lib/core/shared/rcli_installation.rb
@@ -2,33 +2,8 @@ class Rcli
   module Installation
   
     def _installed_rcli_base?
-      File.directory? File.expand_path('~/' + Rcli::APP_CONFIG['global']['hidden_dir_name'] )
+      File.directory? File.expand_path('~/' + Rcli::RCLI_CONFIG['global']['hidden_dir_name'] )
     end
 
-    def _install_rcli_base
-      if _installed_rcli_base?
-        return false
-      end
-
-      dotpath = File.expand_path('~/' + Rcli::APP_CONFIG['global']['hidden_dir_name'] )
-      puts "Installing to : #{dotpath}"
-      # exit
-      FileUtils.mkdir_p(dotpath + DS + 'bin')
-      FileUtils.mkdir_p(dotpath + DS + 'app_info')
-
-      if agree("Add rcli to your path to allow script installations? [yn]", true)
-        File.open(File.expand_path('~/.bash_profile'),'a') { |f|
-          f << "\n\n# rcli Installer addition on #{Time.now.inspect}. Adding appropriate PATH variables for use with rcli."
-          f << "\nexport PATH=#{dotpath}/bin:$PATH"
-          f << "\n# Finished adapting your PATH environment variable for use with rcli."
-        }
-      end
-      
-      File.open(dotpath + DS + 'config.yml', 'w') { |f|
-        f << File.read(Rcli::GEM_CONFIG + DS + 'application.yaml')
-      }
-      
-
-    end
   end
 end
