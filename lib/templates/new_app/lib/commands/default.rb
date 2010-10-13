6564b4c34e3009f34703b95bce95455dbe72dcbf
Eduardo Del Balso <e.delbalso@gmail.com>
2010-10-12 23:53:10 -0400

    Updated template to reflect new description/usage style

--- lib/templates/new_app/lib/commands/default.rb
+++ lib/templates/new_app/lib/commands/default.rb
@@ -1,13 +1,7 @@
 class DefaultCommand  < Command
   
-  def after_init
-    # called after Command#initialize()
-#    @description = "Edit this variable to change the help desription in multi mode"
-  end
-
-  def before_main
-    # called right before the command's #main() function
-  end
+  #description "Your application description (showed in help) goes here"
+  #usage "RCLIAPPNAME [-hv]"
   
   def main
     # script code goes here:
