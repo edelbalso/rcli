3fc1bca39dd53e8c237e45b981122c00a8ee2fdd
Eduardo Del Balso <e.delbalso@gmail.com>
2010-11-09 16:27:43 -0500

    updated version

--- lib/commands/version.rb
+++ lib/commands/version.rb
@@ -3,9 +3,9 @@ class VersionCommand  < Command
   description "Shows the current version of the application."
   
   def main
-    options = parse
+    #options = parse
 
-    puts "#{Rcli.script_config['global']['script_name']} version 0.0.1 (alpha)"
+    puts "#{Rcli.script_config['global']['script_name']} version 0.1.1"
   end
 
 end
